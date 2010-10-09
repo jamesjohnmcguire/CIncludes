4a7931ecade719a775f677acaef86326f45ebbc6
James John McGuire <jamesjohnmcguire@gmail.com>
2010-10-09 11:56:18 +0000

    Include project

---
+++ Common.h
@@ -0,0 +1,20 @@
+#pragma once
+
+///////////////////////////////////////////////////////////////////////
+// Defines
+///////////////////////////////////////////////////////////////////////
+#define IN
+#define OUT
+#define INOUT
+#define RETVAL DWORD
+
+#define DllExport	__declspec( dllexport )
+
+const int GENERAL_ERROR = -1;
+const int CONNECTION_ERROR = -2;
+
+#define E_GENERAL_ERROR		MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x200)
+#define E_CONNECTION_ERROR	MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x201)
+#define E_USER_NOT_FOUND	MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x202)
+#define W_NEW_CERTIFICATE	MAKE_HRESULT(SEVERITY_SUCCESS, FACILITY_ITF, 0x203)
+#define E_USER_CANCEL		MAKE_HRESULT(SEVERITY_ERROR, FACILITY_ITF, 0x204)
